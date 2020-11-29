a7098e1051ebad31e71d4b10384acebd40872cef
Charlie Holtz <charlieholtz@gmail.com>
2020-11-29 14:29:24 -0500

    heroku buildpack stuff

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :shlinkedin, ShlinkedinWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "shlinked.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/shlinkedin_web/endpoint.ex
+++ lib/shlinkedin_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ShlinkedinWeb.Endpoint do
   ]
 
   socket "/socket", ShlinkedinWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
