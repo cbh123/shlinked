703829b0044bbcd641e134e2e07ae95fc4d875e8
Charlie Holtz <charlieholtz@gmail.com>
2020-12-09 23:04:02 -0500

    redirect

--- lib/shlinkedin_web/controllers/user_auth.ex
+++ lib/shlinkedin_web/controllers/user_auth.ex
@@ -134,7 +134,7 @@ defmodule ShlinkedinWeb.UserAuth do
       conn
       |> put_flash(:error, "You must log in to access this page.")
       |> maybe_store_return_to()
-      |> redirect(to: Routes.user_session_path(conn, :new))
+      |> redirect(to: Routes.user_session_path(conn, :join))
       |> halt()
     end
   end
