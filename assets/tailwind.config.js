5f93f9d10ea20c39b0950be850188a8338e5a9a6
Charlie Holtz <charlieholtz@gmail.com>
2020-12-10 22:26:46 -0500

    unlimited likes

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,17 +7,7 @@ module.exports = {
       windows: ["VT323"],
       mono: ["ui-monospace", "SFMono-Regular"],
     },
-    extend: {
-      animation: {
-        wiggle: "wiggle 1s ease-in-out infinite",
-      },
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {
