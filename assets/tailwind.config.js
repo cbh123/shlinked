c06800cabf77b3d2a0d341cdb23800a25fc7d136
Charlie Holtz <charlieholtz@gmail.com>
2020-12-10 21:55:29 -0500

    remove purge

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  purge: [
-    "../lib/**/*.ex",
-    "../lib/**/*.leex",
-    "../lib/**/*.eex",
-    "./js/**/*.js",
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
