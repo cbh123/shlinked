7057f3617e1d981e68cf264e1e242090d79ba4d1
Charlie Holtz <charlieholtz@gmail.com>
2020-12-13 19:12:44 -0500

    better form component, change button name

--- lib/shlinkedin_web/live/profile_live/show.ex
+++ lib/shlinkedin_web/live/profile_live/show.ex
@@ -4,20 +4,19 @@ defmodule ShlinkedinWeb.ProfileLive.Show do
   alias Shlinkedin.Profiles
   alias Shlinkedin.Profiles.Endorsement
 
-
   def mount(%{"slug" => slug}, session, socket) do
     show_profile = Shlinkedin.Profiles.get_profile_by_slug(slug)
 
     # KNOWN BUG: RIGHT WHEN YOU CREATE AN ACCOUNT, THIS BUTTON DOESN"T WORK! PROBLABLY NOT LOADED INTO SOCKET!
     socket = is_user(session, socket)
 
-    {:ok, socket
+    {:ok,
+     socket
      |> assign(live_action: :show)
      |> assign(show_profile: show_profile)
      |> assign(from_profile: socket.assigns.profile)
      |> assign(to_profile: show_profile)
-     |> assign(endorsements: list_endorsements(show_profile.id))
-    }
+     |> assign(endorsements: list_endorsements(show_profile.id))}
   end
 
   @impl true
@@ -39,10 +38,9 @@ defmodule ShlinkedinWeb.ProfileLive.Show do
     socket
     |> assign(:page_title, "Endorse #{socket.assigns.show_profile.persona_name}")
     |> assign(:from_profile, socket.assigns.profile)
-    |> assign(:to_user, Profiles.get_profile_by_slug(slug))
+    |> assign(:to_profile, Profiles.get_profile_by_slug(slug))
     |> assign(:endorsement, %Endorsement{})
   end
-
 
   @impl true
   def handle_event("delete-endorsement", %{"id" => id}, socket) do
