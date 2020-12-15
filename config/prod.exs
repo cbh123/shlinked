4da872292020c5125289adedb4e146f00cc51a41
Charlie Holtz <charlieholtz@gmail.com>
2020-12-15 13:33:19 -0500

    idk

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ config :shlinkedin, ShlinkedinWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "shlinkedin.gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["shlinkedin.gigalixirapp.com"]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
