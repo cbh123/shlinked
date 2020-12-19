d0ccd8480e32922bec94b3b5d3984cdbe538b7ff
Charlie Holtz <charlieholtz@gmail.com>
2020-12-18 22:08:40 -0500

    new comment route

--- lib/shlinkedin_web/router.ex
+++ lib/shlinkedin_web/router.ex
@@ -74,9 +74,9 @@ defmodule ShlinkedinWeb.Router do
     live "/", PostLive.Index, :index
     live "/posts/new", PostLive.Index, :new
     live "/posts/:id/", PostLive.Show, :show
-    live "/posts/:id/:notifications", PostLive.Show, :show
     live "/posts/:id/edit", PostLive.Index, :edit
     live "/posts/:id/new_comment", PostLive.Index, :new_comment
+    live "/posts/:id/:notifications", PostLive.Show, :show
     live "/posts/:id/show/edit", PostLive.Show, :edit
     live "/posts/:id/likes", PostLive.Index, :show_likes
 
