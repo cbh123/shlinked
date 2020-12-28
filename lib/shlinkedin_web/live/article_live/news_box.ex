e3ed210429d8ce22721dd2640607d0c64e5240d5
Charlie Holtz <charlieholtz@gmail.com>
2020-12-28 14:30:51 -0500

    more headlines update

--- lib/shlinkedin_web/live/article_live/news_box.ex
+++ lib/shlinkedin_web/live/article_live/news_box.ex
@@ -1,20 +1,8 @@
 defmodule ShlinkedinWeb.ArticleLive.NewsBox do
   use ShlinkedinWeb, :live_component
-  alias ShlinkedinWeb.ArticleLive.NewsBox
 
   def handle_event("clap", _params, socket) do
     Shlinkedin.News.create_vote(socket.assigns.profile, socket.assigns.article)
-
-    send_update(NewsBox,
-      id: socket.assigns.article.id,
-      spin: true
-    )
-
-    send_update_after(
-      NewsBox,
-      [id: socket.assigns.article.id, spin: false],
-      1000
-    )
 
     {:noreply, socket}
   end
