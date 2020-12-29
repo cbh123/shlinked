c9bc105f7f184497591344981de664340635ed1b
Charlie Holtz <charlieholtz@gmail.com>
2020-12-29 17:01:49 -0500

    janky link

--- lib/shlinkedin_web/live/search_live/search_box.ex
+++ lib/shlinkedin_web/live/search_live/search_box.ex
@@ -4,11 +4,28 @@ defmodule ShlinkedinWeb.SearchLive.SearchBox do
   def mount(_params, session, socket) do
     socket = is_user(session, socket)
 
-    {:ok, assign(socket, query: nil, loading: false, matches: [])}
+    {:ok, assign(socket, query: nil, loading: false, matches: [], show_search: false)}
+  end
+
+  def handle_event("pick", %{"slug" => slug}, socket) do
+    IO.inspect(slug, label: "slug")
+
+    {:noreply,
+     socket
+     |> assign(query: nil, matches: [])
+     |> push_redirect(to: Routes.profile_show_path(socket, :show, slug))}
   end
 
   def handle_event("suggest", %{"q" => q}, socket) do
-    {:noreply, assign(socket, matches: Shlinkedin.Profiles.search_profiles(q))}
+    if q == "" do
+      {:noreply, assign(socket, matches: [])}
+    else
+      {:noreply, assign(socket, matches: Shlinkedin.Profiles.search_profiles(q))}
+    end
+  end
+
+  def handle_event("show-search", _params, socket) do
+    {:noreply, assign(socket, show_search: true)}
   end
 
   def handle_event("search", %{"q" => q}, socket) do
