5f00a95bcd0f19de54847b3dda107aff76639572
Charlie Holtz <charlieholtz@gmail.com>
2020-12-29 17:56:28 -0500

    profile bug

--- lib/shlinkedin_web/live/search_live/search_box.ex
+++ lib/shlinkedin_web/live/search_live/search_box.ex
@@ -7,15 +7,6 @@ defmodule ShlinkedinWeb.SearchLive.SearchBox do
     {:ok, assign(socket, query: nil, loading: false, matches: [], show_search: false)}
   end
 
-  def handle_event("pick", %{"slug" => slug}, socket) do
-    IO.inspect(slug, label: "slug")
-
-    {:noreply,
-     socket
-     |> assign(query: nil, matches: [])
-     |> push_redirect(to: Routes.profile_show_path(socket, :show, slug))}
-  end
-
   def handle_event("suggest", %{"q" => q}, socket) do
     if q == "" do
       {:noreply, assign(socket, matches: [])}
@@ -26,6 +17,10 @@ defmodule ShlinkedinWeb.SearchLive.SearchBox do
 
   def handle_event("show-search", _params, socket) do
     {:noreply, assign(socket, show_search: true)}
+  end
+
+  def handle_event("hide-search", _params, socket) do
+    {:noreply, assign(socket, show_search: false)}
   end
 
   def handle_event("search", %{"q" => q}, socket) do
