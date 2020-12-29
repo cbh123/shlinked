c1ccd3ca41e001985781afc18ce3e6eafce8c460
Charlie Holtz <charlieholtz@gmail.com>
2020-12-28 21:56:54 -0500

    show post title

--- lib/shlinkedin_web/live/post_live/show.ex
+++ lib/shlinkedin_web/live/post_live/show.ex
@@ -11,12 +11,15 @@ defmodule ShlinkedinWeb.PostLive.Show do
 
   @impl true
   def handle_params(%{"id" => id} = params, _, socket) do
+    post = Timeline.get_post_preload_all(id)
+
     {:noreply,
      socket
      |> assign(:from_notifications, Map.has_key?(params, "notifications"))
      |> assign(:page_title, socket.assigns.live_action)
      |> assign(like_map: Timeline.like_map())
      |> assign(show_like_options: false)
-     |> assign(:post, Timeline.get_post_preload_all(id))}
+     |> assign(:post, post)
+     |> assign(:page_title, "See #{post.profile.username}'s post on ShlinkedIn")}
   end
 end
