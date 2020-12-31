f17073c5a62156ca33a8b7f299efded0de9ef654
Charlie Holtz <charlieholtz@gmail.com>
2020-12-31 13:13:08 -0500

    bad shit

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
