af4dbc1c03a695e56d85fb3a2fe5b9b69506de31
Charlie Holtz <charlieholtz@gmail.com>
2021-01-19 16:26:06 -0500

    push to gigalixir

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 database_url =
-  System.get_env("DATABASE_URL") ||
+  System.get_env("AWS_DATABASE_URL") ||
     raise """
     environment variable DATABASE_URL is missing.
     For example: ecto://USER:PASS@HOST/DATABASE
