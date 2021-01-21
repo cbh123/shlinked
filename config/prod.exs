9736f26b830da906caa3c67fa40a359dc6e2121a
Charlie Holtz <charlieholtz@gmail.com>
2021-01-21 18:57:19 -0500

    trying updated host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :shlinkedin, ShlinkedinWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: nil, port: 443],
+  url: [scheme: "https", host: "www.shlinkedin.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
