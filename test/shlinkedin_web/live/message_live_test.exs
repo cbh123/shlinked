24d0bb5380a296dafa5dd5144529895ac4552ca0
Charlie Holtz <charlieholtz@gmail.com>
2021-06-03 14:31:33 -0400

    testing

--- test/shlinkedin_web/live/message_live_test.exs
+++ test/shlinkedin_web/live/message_live_test.exs
@@ -72,4 +72,34 @@ defmodule Shlinkedin.MessageLiveTest do
 
     assert render(view) =~ "hi there"
   end
+
+  test "select someone to message and then message them preservers order", %{conn: conn} do
+    {:ok, view, html} = live(conn, Routes.message_index_path(conn, :index))
+    other = profile_fixture()
+
+    assert html =~ "ShlinkMail"
+
+    view |> element("a", "New") |> render_click()
+
+    assert_patch(view, Routes.message_index_path(conn, :new_message))
+
+    # assert we can search profiles
+    assert view |> element("#search-profiles") |> render_change(%{"q" => "Cha"}) =~ "Charlie"
+
+    {:ok, view, _html} =
+      view
+      |> element("#result-#{other.id}")
+      |> render_click()
+      |> follow_redirect(conn)
+
+    Enum.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], fn i ->
+      view
+      |> element("#send-message")
+      |> render_submit(%{"message" => %{"content" => "number #{i}"}})
+    end)
+
+    assert view |> render() =~ "number 1"
+
+    # {:ok, view, html} = live(conn, Routes.message_show_path(conn, :show, ))
+  end
 end
