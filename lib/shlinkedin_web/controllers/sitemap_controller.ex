d2d7fd745aa30fdc05bbe3fa3ce257c0a811e675
Charlie Holtz <charlieholtz@gmail.com>
2021-08-14 18:44:52 -0400

    sitemap

---
+++ lib/shlinkedin_web/controllers/sitemap_controller.ex
@@ -0,0 +1,10 @@
+defmodule ShlinkedinWeb.SitemapController do
+  use ShlinkedinWeb, :controller
+  import Phoenix.Controller
+
+  def sitemap(conn, _params) do
+    conn
+    |> put_resp_content_type("text/xml")
+    |> render("sitemap.xml")
+  end
+end
