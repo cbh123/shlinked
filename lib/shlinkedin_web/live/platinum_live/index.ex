cdd008dfc961200a002a48f52bea0361442f8b1a
Charlie Holtz <charlieholtz@gmail.com>
2021-10-18 09:59:00 -0400

    platinum social image

--- lib/shlinkedin_web/live/platinum_live/index.ex
+++ lib/shlinkedin_web/live/platinum_live/index.ex
@@ -3,6 +3,27 @@ defmodule ShlinkedinWeb.PlatinumLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign(
+       meta_attrs: [
+         %{
+           property: "og:image",
+           content: "https://shlinked.s3.amazonaws.com/platinum_social.png"
+         },
+         %{
+           name: "twitter:image:src",
+           content: "https://shlinked.s3.amazonaws.com/platinum_social.png"
+         },
+         %{
+           property: "og:image:height",
+           content: "630"
+         },
+         %{
+           property: "og:image:width",
+           content: "1200"
+         }
+       ]
+     )}
   end
 end
