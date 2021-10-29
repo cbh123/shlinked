124bbe5ad72b975932030d3d3dce25fbdf9fc08b
Charlie Holtz <charlieholtz@gmail.com>
2021-10-29 12:51:27 -0400

    tests

--- test/shlinkedin_web/controllers/profile_controller_test.exs
+++ test/shlinkedin_web/controllers/profile_controller_test.exs
@@ -1,7 +1,6 @@
 defmodule ShlinkedinWeb.ProfileControllerTest do
   use ShlinkedinWeb.ConnCase
 
-  alias Shlinkedin.Profiles
   alias Shlinkedin.Profiles.Profile
   import Shlinkedin.ProfilesFixtures
 
@@ -11,13 +10,8 @@ defmodule ShlinkedinWeb.ProfileControllerTest do
 
   describe "show_profile" do
     test "shows profile data", %{conn: conn, profile: %Profile{slug: slug} = profile} do
-      conn = get(conn, Routes.profile_path(conn, :show, profile))
+      conn = get(conn, Routes.profile_path(conn, :show, profile.slug))
       assert %{"slug" => ^slug} = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, profile: profile} do
-      conn = get(conn, Routes.profile_path(conn, :show, "bloop"))
-      assert json_response(conn, 422)["errors"] != %{}
     end
   end
 end
