35bfdcbe5b62e7c9631fe46e7d358ed38f0e625a
Charlie Holtz <charlieholtz@gmail.com>
2021-10-29 13:07:18 -0400

    fix tests

--- test/shlinkedin_web/controllers/profile_controller_test.exs
+++ test/shlinkedin_web/controllers/profile_controller_test.exs
@@ -9,9 +9,9 @@ defmodule ShlinkedinWeb.ProfileControllerTest do
   end
 
   describe "show_profile" do
-    test "shows profile data", %{conn: conn, profile: %Profile{slug: slug} = profile} do
-      conn = get(conn, Routes.profile_path(conn, :show, profile.slug))
-      assert %{"slug" => ^slug} = json_response(conn, 200)["data"]
+    test "shows profile data", %{conn: conn, profile: %Profile{slug: slug, id: id} = profile} do
+      conn = get(conn, Routes.profile_path(conn, :show, slug))
+      assert %{"id" => ^id} = json_response(conn, 200)["data"]
     end
   end
 end
