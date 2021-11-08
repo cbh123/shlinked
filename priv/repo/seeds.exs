1db775e12c5f9c63d04c289f6e38ab3c4789e282
Charlie Holtz <charlieholtz@gmail.com>
2021-11-08 19:10:55 -0400

    fix crypto

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Shlinkedin.Timeline
+
+# create user / profile
+{:ok, user} = Shlinkedin.Accounts.register_user(%{email: "god@shlinkedin.com", password: "bloop"})
+
+{:ok, profile} =
+  create_profile(user, %{
+    "persona_name" => "god",
+    "slug" => "god",
+    "title" => "god",
+    "username" => "god"
+  })
+
+# create a post
+{:ok, _post} = Timeline.create_post(profile, %{body: "first"}, %Timeline.Post{})
+
+_post = create_post(3) |> add_likes()
+
+# helper for adding likes
+defp add_likes(profile, num_likes) do
+  Enum.each(
+    1..num_likes,
+    fn _num ->
+      Timeline.create_like(Shlinkedin.ProfilesFixtures.profile_fixture(), post, "nice")
+    end
+  )
+
+  post
+end
