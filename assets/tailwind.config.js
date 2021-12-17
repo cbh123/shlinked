56eabc770538b342dfdfaddad177d599133a278b
Charlie Holtz <charlieholtz@gmail.com>
2021-12-17 10:31:22 -0500

    business card

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,10 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: [
-    './js/**/*.js',
-    '../lib/*_web/**/*.*ex'
-  ],
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
