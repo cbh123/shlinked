6b6c855efc35c23ba9f54469117209158299e1d5
Charlie Holtz <charlieholtz@gmail.com>
2022-08-28 14:16:19 -0400

    fly stuff

--- config/prod.exs
+++ config/prod.exs
@@ -54,6 +54,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
-
 config :appsignal, :config, active: true
