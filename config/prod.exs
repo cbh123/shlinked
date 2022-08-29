f8588ce6912e256a6c3abd0fa0795e26e5de9a41
Charlie Holtz <charlieholtz@gmail.com>
2022-08-29 08:46:29 -0400

    remove fly.io changes

--- config/prod.exs
+++ config/prod.exs
@@ -54,4 +54,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
+import_config "prod.secret.exs"
 config :appsignal, :config, active: true
